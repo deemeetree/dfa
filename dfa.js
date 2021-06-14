055ad89f54b0e025e388c0805321b23d4f32056c
Dmitry <info@noduslabs.com>
2021-06-14 14:44:54 +0200

    removed debug info from dfa.js

--- dfa.js
+++ dfa.js
@@ -507,8 +507,3 @@
         }
         return eqParts.join(' + ');
     };
-
-    let time_series = [8, 10, 6, 9, 7, 5, 5, 11, 11, 8, 6, 7, 9, 10, 7, 9]
-    let dfa = new DFA(time_series)
-    let alpha_component = dfa.compute()
-    console.log(alpha_component)
